a242fe7738d415e53fb37678ebf436a41206fffd
ayaelsaid <ayae7675@gmail.com>
2024-02-28 22:42:26 -0800

    dele root

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delete binary tree
+ * @tree: pointer to root node
+ * Return: ....
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return;
+	}
+	else
+	{
+		
+		if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
+	free (tree);
+	}
+}
