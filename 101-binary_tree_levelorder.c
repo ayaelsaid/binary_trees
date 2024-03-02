3bc36a2e94beb2fa86c6e93f438f9acfbe2482ef
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-03-02 05:48:04 +0200

    Update binary_trees.h

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,20 +7,24 @@
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (!tree || !func)
-                return;
-        int i = 0, j = 0;
-        binary_tree_t **mycol;
+	if (!tree || !func)
+		return;
+	int i = 0, j = 0;
+	binary_tree_t **mycol;
 
-        mycol = (binary_tree_t **)malloc(sizeof(binary_tree_t));
-        if (mycol == NULL)
-                return NULL;
-        mycol[j++] = tree;
-        while(i < j)
-        {
-                binary_tree_t *temp = mycol[i++];
-                func(temp->n);
-                if (temp->left)
-                        mycol[j++] = temp->left;
-                if (tree->right)
-                        mycol[j++] = temp->right;
+	mycol = (binary_tree_t **)malloc(sizeof(binary_tree_t));
+	if (mycol == NULL)
+		return;
+	mycol[j++] = tree;
+	while (i < j)
+	{
+		binary_tree_t *temp = mycol[i++];
+
+		func(temp->n);
+		if (temp->left)
+			mycol[j++] = temp->left;
+		if (tree->right)
+			mycol[j++] = temp->right;
+	}
+	free(mycol);
+}
